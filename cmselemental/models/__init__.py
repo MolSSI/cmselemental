292e2a53d0bef5053926e2f6a9eff1f29ad707f6
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-23 15:39:23 -0400

    add imports

--- cmselemental/models/__init__.py
+++ cmselemental/models/__init__.py
@@ -1,3 +1,12 @@
-from . import base
+try:
+    import pydantic
+except ImportError:  # pragma: no cover
+    raise ImportError(
+        "Python module pydantic not found. Solve by installing it: "
+        "`conda install pydantic -c conda-forge` or `pip install pydantic`"
+    )
+
 from . import types
-from . import common
+from .base import *
+from .common import *
+from .procedures import *
