02e0941daa205ab3901a1ec597a836ba25cfc680
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-23 15:52:11 -0400

    add tests for models

---
+++ cmselemental/tests/test_models.py
@@ -0,0 +1,74 @@
+import pytest
+
+from cmselemental.models import (
+    ComputeError,
+    FailedOperation,
+    ProcInput,
+    ProcOutput,
+    ProtoModel,
+    Provenance,
+)
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_repr_provenance(request):
+
+    prov = Provenance(creator="qcel", version="v0.3.2")
+    drop_qcsk(prov, request.node.name)
+
+    assert "qcel" in str(prov)
+    assert "qcel" in repr(prov)
+
+
+def test_repr_compute_error():
+    ce = ComputeError(error_type="random_error", error_message="this is bad")
+
+    assert "random_error" in str(ce)
+    assert "random_error" in repr(ce)
+
+
+def test_repr_failed_op():
+    fail_op = FailedOperation(
+        error=ComputeError(error_type="random_error", error_message="this is bad")
+    )
+    assert (
+        str(fail_op)
+        == """FailedOperation(error=ComputeError(error_type='random_error', error_message='this is bad'))"""
+    )
+
+
+def test_repr_optimization():
+
+    opt = ProcInput(
+        **{
+            "id": "some_id",
+            "engine": "some_engine",
+            "engine_version": "1.0.0",
+        }
+    )
+
+    assert "provenance" in str(opt)
+    assert "schema_name" in str(opt)
+    assert "schema_version" in str(opt)
+
+
+def test_model_custom_repr():
+    class Model(ProtoModel):
+        a: int
+
+        def __repr__(self) -> str:
+            return "Hello world!"
+
+    m = Model(a=5)
+    assert repr(m) == "Hello world!"
+    assert "Model(" in str(m)
+
+    class Model2(ProtoModel):
+        a: int
+
+        def __str__(self) -> str:
+            return "Hello world!"
+
+    m = Model2(a=5)
+    assert "Model2(" in repr(m)
+    assert str(m) == "Hello world!"
