42d3d2545caa30c803edbe6f322f8547e4ec3e38
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-08-16 21:14:00 -0400

    add test for writing to hdf5 files

---
+++ cmselemental/tests/test_hdf.py
@@ -0,0 +1,35 @@
+import pytest
+import numpy
+import cmselemental
+from pathlib import Path
+
+
+using_h5py = pytest.mark.skipif(
+    cmselemental.util.which_import("h5py", return_bool=True) is False,
+    reason="Not detecting module h5py. Install package if necessary and add to envvar PYTHONPATH",
+)
+
+
+@pytest.mark.parametrize(
+    "obj",
+    [
+        {
+            "a": numpy.random.rand(4),
+            "b": numpy.array(5.1111111),
+            "c": numpy.array("hello"),
+            "abcdé": {"a": numpy.random.rand(2), "b": numpy.random.rand(5)},
+            "nested": [(1, 0), (0, 1), (2, 2)],
+        },
+        {
+            "a": numpy.array([(0, 1, 2), (2, 3, 4)], dtype="i8,i8,i8"),
+            "b": numpy.array([("ALA", 1.0, 2.0), ("FF", 3.0, 4.0)], dtype="U3,f8,f8"),
+        },
+    ],
+)
+def test_write(obj):
+    from cmselemental.util.hdf import write_file
+
+    path_to_file = Path("filename.h5")
+    write_file(path_to_file.name, data=obj)
+    assert path_to_file.is_file()
+    path_to_file.unlink()
